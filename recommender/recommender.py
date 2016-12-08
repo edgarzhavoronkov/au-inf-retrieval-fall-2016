c93f2e7d8b65a01ec433c09155b5d4f8fdbc4afe
edgarzhavoronkov <edzhavoronkov@gmail.com>
2016-12-09 01:05:50 +0300

    primitive CLI

--- recommender/recommender.py
+++ recommender/recommender.py
@@ -9,12 +9,33 @@ from sklearn.metrics import pairwise_distances
 
 conn = sqlite3.connect('../data/data.sqlite')
 
-def signup(username):
-    pass
+current_user = None
+
+def is_user_present(username, conn):
+    c = conn.cursor()
+    c.execute("SELECT COUNT(NAME) FROM USERS WHERE NAME=?", (username,))
+    for row in c.fetchall():
+        return row[0] > 0
 
 
-def like(username, track):
-    pass
+def add_user(username, conn):
+    c = conn.cursor()
+    c.execute("INSERT INTO USERS(NAME) VALUES (?)", (username,))
+    conn.commit()
+
+def signup(username, conn):
+    global current_user
+    add_user(username, conn)
+    current_user = username
+
+
+def like(username, artist, track, conn):
+    c = conn.cursor()
+    user_id = get_user_id(username, conn)
+    track_id = get_track_id(track, artist, conn)
+    c.execute("INSERT INTO LIKEDTRACK2USER(USERID, TRACKID) "
+              "VALUES (?, ?)", (str(user_id), str(track_id)))
+    conn.commit()
 
 
 def get_max_uid(conn):
@@ -83,6 +104,13 @@ def get_user_id(name, conn):
     for row in c.fetchall():
         return row[0]
 
+def get_track_id(track, artist, conn):
+    c = conn.cursor()
+    c.execute("SELECT TRACKS.ID "
+              "FROM TRACKS JOIN ARTISTS ON TRACKS.ARTISTID = ARTISTS.ID "
+              "WHERE ARTISTS.NAME=? AND TRACKS.NAME=? LIMIT 1", (artist, track))
+    for row in c.fetchall():
+        return row[0]
 
 def get_track_name(tid, conn):
     c = conn.cursor()
@@ -104,7 +132,10 @@ def get_similar_users_ids(dist, user_id, count, conn):
 
 def get_liked_tracks_ids(user_id, conn):
     c = conn.cursor()
-    c.execute("SELECT TRACKID from LIKEDTRACK2USER JOIN USERS ON USERID=USERS.ID WHERE ID=?", (str(user_id),))
+    c.execute("SELECT TRACKID "
+              "FROM LIKEDTRACK2USER JOIN USERS "
+              "ON USERID=USERS.ID "
+              "WHERE ID=?", (str(user_id),))
     res = []
     for row in c.fetchall():
         res.append(int(row[0]))
@@ -122,30 +153,95 @@ def get_frequencies(similar_users_ids, conn):
 
 def get_artist_name(track_id, conn):
     c = conn.cursor()
-    c.execute("SELECT ARTISTS.NAME FROM ARTISTS JOIN TRACKS ON ARTISTS.ID = TRACKS.ARTISTID WHERE TRACKS.ID=?", (int(track_id),))
+    c.execute("SELECT ARTISTS.NAME "
+              "FROM ARTISTS JOIN TRACKS "
+              "ON ARTISTS.ID = TRACKS.ARTISTID "
+              "WHERE TRACKS.ID=?", (int(track_id),))
     for row in c.fetchall():
         return row[0]
 
 
-if __name__ == '__main__':
-    global conn
-    username = sys.argv[1]
-
+def recommend_by_user(username, conn):
     users, tracks, data = read_data(conn)
     dist = init_dist_if_need(data, '../data/dist.dat')
 
     user_id = get_user_id(username, conn)
     liked_tracks_ids = get_liked_tracks_ids(user_id, conn)
-    similar_users_ids = get_similar_users_ids(dist, user_id, 150, conn)
-    frequencies = get_frequencies(similar_users_ids, conn)
 
-    for track_id in liked_tracks_ids:
-        frequencies[track_id] = 0
+    if len(liked_tracks_ids) == 0:
+        recommend_most_popular(conn)
+    else:
+        similar_users_ids = get_similar_users_ids(dist, user_id, 150, conn)
+        frequencies = get_frequencies(similar_users_ids, conn)
 
-    res_ids = np.flipud(np.argsort(frequencies))[0 : 250]
-    for res_id in res_ids:
-        if (res_id != 0):
-            artist_name = get_artist_name(res_id, conn)
-            track_name = get_track_name(res_id, conn)
-            print(artist_name + " - " + track_name)
+        for track_id in liked_tracks_ids:
+            frequencies[track_id] = 0
+
+        res_ids = np.flipud(np.argsort(frequencies))[0 : 250]
+        for res_id in res_ids:
+            if (res_id != 0):
+                artist_name = get_artist_name(res_id, conn)
+                track_name = get_track_name(res_id, conn)
+                print(artist_name + " - " + track_name)
+
+
+def recommend_most_popular(conn):
+    users, tracks, data = read_data(conn)
+    data = data.tocsc()
+    freqs = data.sum(0).tolist()[0]
+    tops = np.flipud(np.argsort(freqs))[0:10]
+    for top in tops:
+        if (top != 0):
+            artist_name = get_artist_name(top, conn)
+            track_name = get_track_name(top, conn)
+            print(artist_name + " - " + track_name + " (" + str(freqs[top]) +")")
+
+
+def get_tracks_by_artist(artist, conn):
+    res = []
+    c = conn.cursor()
+    c.execute("SELECT DISTINCT(TRACKS.NAME) "
+              "FROM TRACKS JOIN ARTISTS "
+              "ON TRACKS.ARTISTID = ARTISTS.ID "
+              "WHERE ARTISTS.NAME=?", (artist,))
+    for row in c.fetchall():
+        res.append(row[0])
+    return res
+
+
+if __name__ == '__main__':
+    print("Tiny recommender engine")
+    while True:
+        user_input = input(">> ").split(" ")
+
+        if user_input[0] == "signup":
+            username = user_input[1]
+            if is_user_present(username, conn):
+                print("User {0} already present, select another name".format(username))
+            else:
+                signup(username, conn)
+                print("Logged in as {0}".format(username))
+        elif user_input[0] == "like":
+            if current_user is None:
+                print("Sign up to like first!")
+            else:
+                artist = user_input[1]
+                track = " ".join(user_input[2 : ])
+                like(current_user, artist, track, conn)
+
+        elif user_input[0] == "tracks":
+            artist = " ".join(user_input[1:])
+            print('\n'.join(get_tracks_by_artist(artist, conn)))
+
+        elif user_input[0] == "recommend":
+            if current_user is None:
+                recommend_most_popular(conn)
+            else:
+                recommend_by_user(current_user, conn)
+
+        elif user_input[0] == "exit":
+            break
+
+        else:
+            pass
         
